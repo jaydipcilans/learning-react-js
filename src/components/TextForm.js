8cb12ca94200b8ab1c949dc0b24fbb20702669b0
Jaydip <jaydip.cilans@gmail.com>
2023-03-28 16:48:56 +0530

    added dark to light mode and text conveter

---
+++ src/components/TextForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const [text, setText] = useState("")
+
+    const handleUpClick = () => {
+        setText(text.toUpperCase())
+    }
+
+    const handleLoClick = () => {
+        setText(text.toLowerCase())
+    }
+
+    const handleDelete = () => {
+        setText("")
+    }
+
+    // const handleDowenloadText = (e) => {
+    //     let element = document.createElement("a")
+    //     element.href = "data:text/plain," + encodeURIComponent(e);
+    //     element.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(e);
+    //     element.target = "_blank";
+    //     element.download = "textFile.csv";
+    //     element.click()
+    // }
+
+    const handleCopy = () => {
+        let text = document.getElementById('myBox')
+        text.select()
+        navigator.clipboard.writeText(text.value)
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(" "))
+    }
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+
+    return (
+        <>
+            <div className='container'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleChange}>Enter text here</textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleDelete}>Delete</button>
+                {/* <button className="btn btn-primary mx-1" onClick={handleDowenloadText}>Download Text</button> */}
+                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            </div>
+            <div className="container my-3">
+                <h1>Your text summary</h1>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
+    )
+}
